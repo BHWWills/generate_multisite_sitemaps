51828c148e68db3e0fde46baf96c53a34f8bfa53
BHWWills <34718318+BHWWills@users.noreply.github.com>
2026-01-29 08:56:44 +0000

    Live version

--- controller.php
+++ controller.php
@@ -7,7 +7,7 @@ use Concrete\Core\Command\Task\Manager;
 class Controller extends Package
 {
     protected $appVersionRequired = '9.0';
-    protected $pkgVersion = '1.5';
+    protected $pkgVersion = '1.5.2';
     protected $pkgHandle = 'generate_multisite_sitemaps';
     protected $pkgAutoloaderRegistries = [
         'src' => '\Concrete\Package\GenerateMultisiteSitemaps'
